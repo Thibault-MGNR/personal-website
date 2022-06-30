fbd8f8094453bb58f3c2836ca70dc5a21458479c
Thibault <titi76mag@gmail.com>
2022-06-30 23:44:59 +0200

    menu button rotation

--- scripts.js
+++ scripts.js
@@ -1,34 +1,44 @@
-/*
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const square = entry.target.querySelector('.fade-in-animation');
-  
-      if (entry.isIntersecting) {
-        square.classList.add('fade-in');
-        return;
-      }
+function reveal() {
+  var reveals = document.querySelectorAll(".fade-in-container");
 
-      square.classList.remove('fade-in');
-    });
-  });
-  
-  observer.observe(document.querySelector('.fade-in-container')); */
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-  function reveal() {
-    var reveals = document.querySelectorAll(".fade-in-container");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("fade-in");
-        reveals[i].classList.remove("hide")
-      } else {
-        // reveals[i].classList.remove("fade-in");
-      }
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("fade-in");
+      reveals[i].classList.remove("hide");
+    } else {
     }
   }
-  
+}
+
+function menuRotation() {
+  console.log("click");
+  menuButton.classList.remove("rotate");
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      if(menuButtonHover == true){
+        menuButton.classList.add("rotate");
+      }
+    });
+  });
+}
+
+function init(){
   window.addEventListener("scroll", reveal);
+  menuButton = document.getElementById('menu-button');
+  menuButton.addEventListener("click", menuRotation);
+  menuButton.addEventListener("mouseenter", function(event){
+    menuButtonHover = true;
+  });
+
+  menuButton.addEventListener("mouseleave", function(event){
+    menuButtonHover = false;
+  });
+}
+
+var menuButton = null;
+var menuButtonHover = false;
+window.addEventListener("DOMContentLoaded", init);
