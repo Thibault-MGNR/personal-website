ef08348e6a0c985804ff9ac7a2fbe03c8352e48b
Thibault <titi76mag@gmail.com>
2022-07-02 00:37:11 +0200

    menu pop phone

--- scripts.js
+++ scripts.js
@@ -15,16 +15,65 @@ function reveal() {
 }
 
 function menuRotation() {
-
   if(menuIsDevelop){
     menuButton.classList.remove("rotate-menu-f");
     menuButton.classList.add("rotate-menu-b");
+    popOutMenuLi();
+    popOutMenuA();
+    slideOutH1Title();
     menuIsDevelop = false;
   } else {
     menuButton.classList.remove("rotate-menu-b");
     menuButton.classList.add("rotate-menu-f");
+    popInMenuLi();
+    popInMenuA();
+    slideInH1Title();
     menuIsDevelop = true;
   }
+}
+
+function popInMenuLi() {
+  var menu_li = document.querySelectorAll(".nav-button");
+  for(var i = 0; i < menu_li.length; i++){
+    menu_li[i].classList.remove("menu-pop-out-li");
+    menu_li[i].classList.add("menu-pop-in-li");
+  }
+}
+
+function popOutMenuLi() {
+  var menu_li = document.querySelectorAll(".nav-button");
+  for(var i = 0; i < menu_li.length; i++){
+    menu_li[i].classList.remove("menu-pop-in-li");
+    menu_li[i].classList.add("menu-pop-out-li");
+  }
+}
+
+function popInMenuA(){
+  var menu_li_a = document.querySelectorAll(".nav-button a");
+  for(var i = 0; i < menu_li_a.length; i++){
+    menu_li_a[i].classList.remove("fade-out-menu-a");
+    menu_li_a[i].classList.add("fade-in-menu-a");
+  }
+}
+
+function popOutMenuA(){
+  var menu_li_a = document.querySelectorAll(".nav-button a");
+  for(var i = 0; i < menu_li_a.length; i++){
+    menu_li_a[i].classList.remove("fade-in-menu-a");
+    menu_li_a[i].classList.add("fade-out-menu-a");
+  }
+}
+
+function slideInH1Title(){
+  var h1Title = document.getElementById("header-title");
+  h1Title.classList.remove("slideOutH1Title");
+  h1Title.classList.add("slideInH1Title");
+}
+
+function slideOutH1Title(){
+  var h1Title = document.getElementById("header-title");
+  h1Title.classList.remove("slideInH1Title");
+  h1Title.classList.add("slideOutH1Title");
 }
 
 function init(){
@@ -32,7 +81,6 @@ function init(){
   menuButton = document.getElementById('menu-button');
   menuButton.addEventListener("click", menuRotation);
 }
-
 var menuButton = null;
 var menuIsDevelop = false;
 window.addEventListener("DOMContentLoaded", init);
